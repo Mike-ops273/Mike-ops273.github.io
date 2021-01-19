c4ca099a3c2f826963a7b17d293f98ef5f0520ae
Mike-ops273 <mivaxd55@yahoo.com>
2021-01-19 16:46:14 +0200

    update
    done with the structure of the page. Next plan is to work on the 3d cards for the 'what i do' section

--- scipt.js
+++ scipt.js
@@ -1,6 +1,13 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open'); /*toggles between the class name of an element.
     'nav-open' and 'nav-toggle' will be related to one another. */
+}) 
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
 })
